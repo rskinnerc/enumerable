61850a6952bb7625e5b5c5910ddfcc72e30689b8
unknown <skj99667788@gmail.com>
2022-07-15 00:49:32 +0500

    create method each

--- my_list.rb
+++ my_list.rb
@@ -2,6 +2,55 @@ class MyList
   def initialize(*list)
     @list = list
   end
+
+
+  def each 
+    li = 0
+    while li < @list.length
+      yield @list[li]
+      li += 1
+    end
+  end
+
 end
 
 list = MyList.new(1, 2, 3, 4)
+
+
+# Each class and module should has a separate .rb file.
+
+# Create a class MyList that has an instance variable @list.
+
+# In MyList implement a method #each that yields successive members of @list
+
+# mylist method should use the MyEnumerable module.
+
+# Create a module MyEnumerable that implements the following methods 
+# (they should have the same funcionality as methods in Enumerable):
+#all?
+#any?
+#filter
+
+
+
+# after doing all
+
+# # Create our list
+# irb> list = MyList.new(1, 2, 3, 4)
+# => #<MyList: @list=[1, 2, 3, 4]>
+
+# # Test #all?
+# irb> list.all? {|e| e < 5}
+# => true
+# irb> list.all? {|e| e > 5}
+# => false
+
+# # Test #any?
+# irb> list.any? {|e| e == 2}
+# => true
+# irb> list.any? {|e| e == 5}
+# => false
+
+# # Test #filter
+# irb> list.filter {|e| e.even?}
+# => [2, 4]
